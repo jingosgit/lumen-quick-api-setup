ec460227c4485a039d48c468fa02681067966612
jingosgit <jingostudios@gmail.com>
2021-10-08 17:27:37 +0300

    Lumen Basic Install

---
+++ README.md
@@ -0,0 +1,25 @@
+# A Quick Lumen PHP Framework Install
+This is a basic configure Lumen app to quickly start using it with laravel commands
+---
+
+## Installation guide
+
+### 1. Copy .env.example .env
+
+### 2. Execute command Composer install 
+
+### 3. Write php artisan key:generate - to generate app Key
+
+### 4. Launch the app - php artisan serve
+
+---
+
+
+## Lumen Security Vulnerabilities
+
+If you discover a security vulnerability within Lumen, please send an e-mail to Taylor Otwell at taylor@laravel.com. All security vulnerabilities will be promptly addressed.
+
+## License
+
+The Lumen framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SIMPLE APP SETUP</title>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="flex justify-center w-full mt-40">
+	<div class="grid grid-cols-1">
+		
+		<header class="text-gray-600 body-font">
+		  <div class="container mx-auto p-5 text-center">
+		    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+		     <h1 class="text-4xl text-red-600 mx-auto">eSOKONI</h1>
+		    </a>
+		
+		  </div>
+		</header>
+
+
+	
+		<section class="text-gray-600 body-font relative">
+		  <div class="containerpx-5 py-3 mx-auto">
+		    <div class="flex flex-col text-center w-full mb-12">
+		      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Start an API</h1>
+		      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+		     To build an exciting One!</p>
+		     <br/>
+		     <br/>
+		     <a href="http://esokoni.co.ke" class="bg-red-600 text-white p-2 rounded">Build one for me</a>
+		    </div>
+		    <div class="w-full mx-auto">
+		      <div class="flex flex-wrap -m-2">
+		        <div class="p-2 w-full">
+		          <div class="mx-auto">
+		          <!--  -->
+		          
+		    
+		      </div>
+		    </div>
+		  </div>
+		</section>
+	</div>
+</div>
+	
+
+</body>
+</html>
